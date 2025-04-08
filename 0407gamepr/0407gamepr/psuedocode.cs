31e75775a77609d23deb4f891ea267faa313a71b
Dorran13 <dorani0918@naver.com>
2025-04-08 18:03:50 +0900

    pseudo

---
+++ 0407gamepr/0407gamepr/psuedocode.cs
@@ -0,0 +1,89 @@
+﻿namespace _0407gamepr
+{
+    class PsuedoCode
+    {
+        // 집 : H scene home
+        // 1. 맵
+        // #########
+        // #F     S#
+        // #       #
+        // #   p   #
+        // #########
+
+        // 상점 : S scene shop
+        // 1. 창으로 띄운다
+        // 2. 선택지를 제공한다
+
+        // 전투장소 : F scene field
+        // 1. 맵
+        // #########
+        // #   m   #
+        // #       #
+        // #B  p  H#
+        // #########
+
+        // 보스방 : B scene boss
+        // 1. 맵
+        // #########
+        // #   o   #
+        // #       #
+        // #F     p#
+        // #########
+
+        // 전투장면 scene battle()
+        // 1. 슬라임 vs 플레이어
+        // 2. 고블린 vs 플레이어
+        // 3. 오크 vs 플레이어
+        // 4. 선택지를 제공한다
+
+        // #출현대상
+        // 플레이어 : p
+        // hp : 100 attack : 20 armor : 0
+        // #크리쳐
+        // 고블린 : g
+        // hp : 60 attack : 5 armor : 0
+        // 슬라임 : s
+        // hp : 40 attack : 5 armor : 5
+        // 보스(오크) : o
+        // hp : 120 attack : 20 armor : 20
+
+        // #장비
+        // 무기
+        // attack + 20
+        // 방어구
+        // armor + 20
+        // 아이템
+        // hp포션
+        // hp + 50
+        // 인벤토리
+        // gold 골드
+        // potion 포션
+        // equip 장착
+        // have 소지
+
+        // 객체
+        // class main
+        // start, run, end
+        // class game
+        // class scene
+        // home, shop, field, boss, slime, goblin, orc
+        // class player
+        // hp, attack, armor, potion, gold, weapon, armor
+        // class monster
+        // slime, goblin, orc
+        // hp, attack, armor, special attack
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 0407gamepr/0407gamepr/titlescene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0407gamepr
+{
+    public class TitleScene
+    {
+        public static void Home()
+        {
+            Console.Write("간단한 턴제 전투게임 시뮬레이션/t");
+            Console.WriteLine("feat 장비 망겜");
+        }
+        
+    }
+}
